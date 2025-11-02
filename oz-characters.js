5568c505f6dcb5b029e8310b4967043375d2b44d
Angelica StarLux <angelicastarlux@gmail.com>
2025-11-02 14:32:59 -0600

    Update Dorothy Kisses Tin Man character to use new dorothykisstinman.png image

--- oz-characters.js
+++ oz-characters.js
@@ -48,7 +48,7 @@ const characters = [
   { name: "The Crows 🐦‍⬛", image: "https://placeholder-url.com/crows.jpg", category: "Supporting Characters" },
 
   // Miscellaneous
-  { name: "Dorothy Kisses Tin Man Goodbye 💋", image: "https://raw.githubusercontent.com/angelicastarlux/wizard-of-oz-bots/main/images/did-dorothy-in-the-wizard-of-oz-hurt-the-feelings-of-the-v0-3wijm3joko4f1.jpg", category: "Miscellaneous" }
+  { name: "Dorothy Kisses Tin Man Goodbye 💋", image: "https://raw.githubusercontent.com/angelicastarlux/wizard-of-oz-bots/main/images/dorothykisstinman.png", category: "Miscellaneous" }
 
 ];
 module.exports = characters;
